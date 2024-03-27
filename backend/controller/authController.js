4e61582c00b5fef66bc47952c81dfc00ee89866c
Thamilanpu <sthamil1016@gmail.com>
2024-03-27 17:10:29 +0530

    change NODE_ENV

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -12,7 +12,7 @@ const generateAuthToken = (user, res) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
+    // secure: process.env.NODE_ENV !== 'development',
     // sameSite: 'strict',
     sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000,
