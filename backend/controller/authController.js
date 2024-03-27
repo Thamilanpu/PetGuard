746d77b710b0b32ec391a852de5003b9a7a5b038
Thamilanpu <sthamil1016@gmail.com>
2024-03-27 11:25:01 +0530

    change  backend code

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -13,7 +13,8 @@ const generateAuthToken = (user, res) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
+    // sameSite: 'strict',
+    sameSite: 'None',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
